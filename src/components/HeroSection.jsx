39cf34916f8a5b574c71de25a70539171474c565
dodunsoftsolutions <dodunsoftsolutions@gmail.com>
2024-09-17 00:11:36 +0530

    background

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
         className="absolute top-0 left-0 w-full h-full object-cover"
       >
         <source
-          src="https://ik.imagekit.io/7uve7qsipm/Copy%20of%20dodun%20.mp4?updatedAt=1726160783516" // Replace with your video URL
+          src="https://ik.imagekit.io/tdlebsr5e/dodun%20website%20%20(1).mp4?updatedAt=1726486076489" // Replace with your video URL
           type="video/mp4"
         />
         Your browser does not support the video tag.
